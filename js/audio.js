4bb57416802f43e3f02e4374664e7d19ab5e94c3
ferrymanspace <tianci_1080@qq.com>
2021-03-17 20:00:44 +0800

    Site updated: 2021-03-17 20:00:43

--- js/audio.js
+++ js/audio.js
@@ -2,7 +2,7 @@
     "use strict";
     function playWhenLoaded() {
         if (window.location.pathname == '/') {
-            new Audio('/audio/index.wav').play();
+            new Audio('https://link.jscdn.cn/sharepoint/aHR0cHM6Ly92aWN0b3JpYWNhLW15LnNoYXJlcG9pbnQuY29tLzp1Oi9nL3BlcnNvbmFsL3RjY2ZfbmNvdl9vZmZpY2VfZ3kvRWNZUjlnZTVsNGxJdE1zTkpzSUtobkVCRG9EUVRMVWVTdkZ3NDhNLVI5aE5SUT9lPWNWeHhkMQ==.wav').play();
         }
     }
     if (document.readyState === 'loading') {
